0e5303c7624dd758c05ca149e5fd6dbf85c18efd
wowns-dl <87560454+wowns-dl@users.noreply.github.com>
2021-12-11 22:58:38 +0900

    Update 계산기1.py

--- 계산기1.py
+++ 계산기1.py
@@ -10,20 +10,66 @@ def one():
     global now
     now +=1
     label.config(text=str(now))
+    
+def two():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
+def three():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
+def four():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
+def five():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
+def six():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
+def seven():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
+def eight():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
+def nine():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
+def zero():
+    global now
+    now +=1
+    label.config(text=str(now))
+    
 
 label = tkinter.Label(window, text="0",width=5,height=5)
 label.place(x=128,y=0)
 
 button1 = tkinter.Button(window,text='1', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button2 = tkinter.Button(window,text='2', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button3 = tkinter.Button(window,text='3', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button4 = tkinter.Button(window,text='4', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button5 = tkinter.Button(window,text='5', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button6 = tkinter.Button(window,text='6', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button7 = tkinter.Button(window,text='7', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button8 = tkinter.Button(window,text='8', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button9 = tkinter.Button(window,text='9', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
-button0 = tkinter.Button(window,text='0', overrelief="solid", width=11,height=5, command=one, repeatdelay=1000, repeatinterval=100)
+button2 = tkinter.Button(window,text='2', overrelief="solid", width=11,height=5, command=two, repeatdelay=1000, repeatinterval=100)
+button3 = tkinter.Button(window,text='3', overrelief="solid", width=11,height=5, command=three, repeatdelay=1000, repeatinterval=100)
+button4 = tkinter.Button(window,text='4', overrelief="solid", width=11,height=5, command=four, repeatdelay=1000, repeatinterval=100)
+button5 = tkinter.Button(window,text='5', overrelief="solid", width=11,height=5, command=five, repeatdelay=1000, repeatinterval=100)
+button6 = tkinter.Button(window,text='6', overrelief="solid", width=11,height=5, command=six, repeatdelay=1000, repeatinterval=100)
+button7 = tkinter.Button(window,text='7', overrelief="solid", width=11,height=5, command=seven, repeatdelay=1000, repeatinterval=100)
+button8 = tkinter.Button(window,text='8', overrelief="solid", width=11,height=5, command=eight, repeatdelay=1000, repeatinterval=100)
+button9 = tkinter.Button(window,text='9', overrelief="solid", width=11,height=5, command=nine, repeatdelay=1000, repeatinterval=100)
+button0 = tkinter.Button(window,text='0', overrelief="solid", width=11,height=5, command=zero, repeatdelay=1000, repeatinterval=100)
 button1.place(x=0,y=100)
 button2.place(x=100,y=100)
 button3.place(x=200,y=100)
